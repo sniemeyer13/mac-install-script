b97cf58d6175e84f1b00fe9459bd71263c07cdad
Sarah Niemeyer <sniemeyer@pivotal.io>
2018-04-25 09:34:03 -0600

    Only install if not yet installed

--- mac_install.sh
+++ mac_install.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
+mkdir -p ~/workspace ~/Downloads
+
+if [ ! -f /usr/local/bin/brew ]; then
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-mkdir ~/workspace
-xcode-select --install
+fi
+
 brew tap caskroom/versions
-brew cask install java8
-brew install cloudfoundry/tap/cf-cli
-brew install mysql
-brew install gradle
-brew install flyway
-brew install graphviz
-brew install rabbitmq
-brew install curl
+brew cask list java8 &>/dev/null || brew cask install java8
+brew list git &>/dev/null || brew install git
+brew list cf-cli &>/dev/null || brew install cloudfoundry/tap/cf-cli
+brew list mysql &>/dev/null || brew install mysql
+brew list gradle &>/dev/null || brew install gradle
+brew list flyway &>/dev/null || brew install flyway
+brew list graphviz &>/dev/null || brew install graphviz
+brew list rabbitmq &>/dev/null || brew install rabbitmq
+brew list curl &>/dev/null || brew install curl
 
-
+if [ ! -f /usr/local/bin/idea ]; then
+    cd ~/Downloads
+    curl -s "https://data.services.jetbrains.com/products/releases?code=IIC&latest=true&type=release" > idea.json
+    LATEST_IDEA=`cat idea.json | jq -r '.IIC[0].downloads.mac.link'`
+    curl -O ${LATEST_IDEA}
+fi

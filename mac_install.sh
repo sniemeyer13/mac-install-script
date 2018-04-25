4f86baaebd60ad8e9988d542a50f7005bda7bb1e
Sarah Niemeyer <sniemeyer@pivotal.io>
2018-04-25 08:17:34 -0600

    Initial commit

---
+++ mac_install.sh
@@ -0,0 +1,14 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+mkdir ~/workspace
+xcode-select --install
+brew tap caskroom/versions
+brew cask install java8
+brew install cloudfoundry/tap/cf-cli
+brew install mysql
+brew install gradle
+brew install flyway
+brew install graphviz
+brew install rabbitmq
+brew install curl
+
+
